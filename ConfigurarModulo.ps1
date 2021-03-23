c3f5d5dd0030891d20919336f276569da8a2993f
Ivo Dias <igd753@gmail.com>
2021-03-23 16:53:55 -0300

    Update ConfigurarModulo.ps1

--- ConfigurarModulo.ps1
+++ ConfigurarModulo.ps1
@@ -1,13 +1,15 @@
-Import-Module 
-
 try {
     # Configuracoes
-    $urlDownload = ""
+    $urlDownload = "https://github.com/IGDEXE/Veracode/blob/8b05a87a08b0d06c7b2fc4ec7a757939fed3934d/Modulo/VeracodeTools.psm1"
+    $pastaModulos = "C:\Program Files\WindowsPowerShell\Modules"
+    $caminhoModulo = "$pastaModulos/VeracodeTools.psm1"
     # Faz o download dos modulos
-    Invoke-WebRequest -Uri "$urlDownload" -OutFile "$env:LOCALAPPDATA/VeracodeTools.zip"
-    # Descompacta arquivos
-    Expand-Archive -Path "$env:LOCALAPPDATA/VeracodeTools.zip" -DestinationPath "C:\Program Files\WindowsPowerShell\Modules"
+    Invoke-WebRequest -Uri "$urlDownload" -OutFile "$caminhoModulo"
+    # Faz a importacao
+    Import-Module -Name "$caminhoModulo" -Verbose
 }
 catch {
-    
+    $ErrorMessage = $_.Exception.Message # Recebe o erro
+    Write-Host "Erro ao importar o modulo"
+    Write-Host "$ErrorMessage"
 }
