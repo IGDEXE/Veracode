fe25c3b0d7d9441cb46ce9dabde2b943c5a08cf8
Ivo Dias <igd753@gmail.com>
2021-09-13 20:10:21 -0300

    Function for PreScan Size

--- Windows/PreScan tamanhos.ps1
+++ Windows/PreScan tamanhos.ps1
@@ -1,64 +1,70 @@
-# Configuracoes
-$veracodeID = ""
-$veracodeAPIkey = ""
-$perfilValidacao = ""
-$caminhoLOG = ""
-$nomeProjeto = ""
-$caminhoArquivo = ""
-$numeroVersao = Get-Date -Format hhmmssddMMyy
 
-# Recebe o App ID com base no nome da aplicacao dentro do Veracode
-[xml]$INFO = $(VeracodeAPI.exe -vid "$veracodeID" -vkey "$veracodeAPIkey" -action GetAppList | Select-String -Pattern $perfilValidacao)
-# Filtra o App ID
-$appID = $INFO.app.app_id
+function Get-VeracodeAppSize {
+    param (
+        $veracodeID = "",
+        $veracodeAPIkey = "",
+        $nomeProjeto = "",
+        $caminhoArquivo = "",
+        $caminhoLOG = "$env:LOCALAPPDATA\$nomeProjeto.txt",
+        $perfilValidacao = ""
+    )
+
+    # Configuracoes
+    $numeroVersao = Get-Date -Format hhmmssddMMyy
+
+    # Recebe o App ID com base no nome da aplicacao dentro do Veracode
+    [xml]$INFO = $(VeracodeAPI.exe -vid "$veracodeID" -vkey "$veracodeAPIkey" -action GetAppList | Select-String -Pattern $perfilValidacao)
+    # Filtra o App ID
+    $appID = $INFO.app.app_id
 
 
-# Deleta a ultima e cria uma nova build
-VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action deletebuild -appid $appID
-VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action createbuild -appid $appID -version $numeroVersao
+    # Deleta a ultima e cria uma nova build
+    VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action deletebuild -appid $appID
+    VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action createbuild -appid $appID -version $numeroVersao
 
-# Faz o upload do arquivo
-VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action uploadfile -appid "$appID" -filepath "$caminhoArquivo"
+    # Faz o upload do arquivo
+    VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action uploadfile -appid "$appID" -filepath "$caminhoArquivo"
 
-# Faz o pre scan no perfil de validacao
-VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action beginprescan -appid "$appID"
+    # Faz o pre scan no perfil de validacao
+    VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action beginprescan -appid "$appID"
 
-# Pega os resultados
-try {
-    # Configuracoes do loop
-    [int]$contador = 10
-    [int]$hardcount = 0
-    if ($hardcount -le 300) {
-        do {
-            VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action GetPreScanResults -appid "$appID" >> "$env:LOCALAPPDATA\$numeroVersao.txt"
-            $retorno = Get-Content "$env:LOCALAPPDATA\$numeroVersao.txt"
+    # Pega os resultados
+    try {
+        # Configuracoes do loop
+        [int]$contador = 10
+        [int]$hardcount = 0
+        if ($hardcount -le 300) {
+            do {
+                VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action GetPreScanResults -appid "$appID" >> "$env:LOCALAPPDATA\$numeroVersao.txt"
+                $retorno = Get-Content "$env:LOCALAPPDATA\$numeroVersao.txt"
+                Clear-Host
+                if ($retorno -match "$appID") {
+                    break
+                } else {
+                    $validacao = $true
+                    Write-Host "Validando se o Scan $numeroVersao já foi concluido"
+                    Write-Host "Por favor aguarde"
+                    Start-Sleep -s $contador
+                    $hardcount += $contador
+                }
+            } while ($validacao)
+
+            # Pega os dados do scan
+            [xml]$preScanInfo = $(VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action getprescanresults -appid $appID)
+            $tamanhos = $preScanInfo.prescanresults.module.size
+            # Faz o log
+            Add-Content -Path $caminhoLOG -Value "Projeto: $nomeProjeto | Tamanho dos modulos: $tamanhos"
+
+        } else {
             Clear-Host
-            if ($retorno -match "$appID") {
-                break
-            } else {
-                $validacao = $true
-                Write-Host "Validando se o Scan $numeroVersao já foi concluido"
-                Write-Host "Por favor aguarde"
-                Start-Sleep -s $contador
-                $hardcount += $contador
-            }
-        } while ($validacao)
-
-        # Pega os dados do scan
-        [xml]$preScanInfo = $(VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action getprescanresults -appid $appID)
-        $tamanhos = $preScanInfo.prescanresults.module.size
-        # Faz o log
-        Write-Host "Projeto: $nomeProjeto | Tamanho dos modulos: $tamanhos"
-
-    } else {
-        Clear-Host
-        Write-Host "Um erro aconteceu ao fazer o procedimento"
-        Write-Host "Favor verificar a mensagem abaixo e comunicar o suporte"
-        VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action GetPreScanResults -appid "$appID"
+            Write-Host "Um erro aconteceu ao fazer o procedimento"
+            Write-Host "Favor verificar a mensagem abaixo e comunicar o suporte"
+            VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action GetPreScanResults -appid "$appID"
+        }
     }
-}
-catch {
-    $ErrorMessage = $_.Exception.Message # Recebe o erro
-    Write-Host "Erro ao validar o Scan e pegar os dados"
-    Write-Host "$ErrorMessage"
+    catch {
+        $ErrorMessage = $_.Exception.Message # Recebe o erro
+        Write-Host "Erro ao validar o Scan e pegar os dados"
+        Write-Host "$ErrorMessage"
+    }
 }
