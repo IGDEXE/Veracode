9650bc6b25d73298feda2ee81ee7a00be600e4ba
Ivo Dias <igd753@gmail.com>
2021-09-21 17:46:11 -0300

    Exemplos Windows

--- Windows/Config Ferramentas.ps1
+++ Windows/Config Ferramentas.ps1
@@ -3,15 +3,19 @@ $pastaferramenta  = "$Env:Programfiles/Veracode/" # Define uma pasta onde vamos 
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$pastaferramenta") # Adiciona o caminho no Path do sistema
 
 # Download e configuração: Pipeline Scan
-$urlPipeScan = "https://downloads.veracode.com/securityscan/pipeline-scan-LATEST.zip" # Define a url de download
-Invoke-WebRequest -Uri "$urlDownloadAPI" -OutFile "$env:LOCALAPPDATA/VeracodeAPI.zip" # Faz o download
-Expand-Archive -Path "$env:userprofile/Download/pipescan.zip" -DestinationPath "$pastaferramenta" # Descompacta o ZIP para uma pasta
+$urlDownload = "https://downloads.veracode.com/securityscan/pipeline-scan-LATEST.zip" # Define a url de download
+$caminhoDownload = "$env:LOCALAPPDATA/VeracodePipeline.zip" # Define um caminho para o arquivo de download
+Invoke-WebRequest -Uri "$urlDownload" -OutFile "$caminhoDownload" # Faz o download
+Expand-Archive -Path "$caminhoDownload" -DestinationPath "$pastaferramenta" # Descompacta o ZIP para uma pasta
+Remove-Item "$caminhoDownload" # Remove o arquivo de download
 
 # Download e configuração: API Wrapper
-$urlDownloadAPI = "https://tools.veracode.com/integrations/API-Wrappers/C%23/bin/VeracodeC%23API.zip" # Define a url de download
-Invoke-WebRequest -Uri "$urlDownloadAPI" -OutFile "$env:LOCALAPPDATA/VeracodeAPI.zip" # Faz o download
-Expand-Archive -Path "$env:LOCALAPPDATA/VeracodeAPI.zip" -DestinationPath "$pastaferramenta" # Descompacta o ZIP para uma pasta
+$urlDownload = "https://tools.veracode.com/integrations/API-Wrappers/C%23/bin/VeracodeC%23API.zip" # Define a url de download
+$caminhoDownload = "$env:LOCALAPPDATA/VeracodeAPI.zip" # Define um caminho para o arquivo de download
+Invoke-WebRequest -Uri "$urlDownload" -OutFile "$caminhoDownload" # Faz o download
+Expand-Archive -Path "$caminhoDownload" -DestinationPath "$pastaferramenta" # Descompacta o ZIP para uma pasta
 Rename-Item -Path "$pastaferramenta/VeracodeC#API.exe" -NewName "$pastaferramenta/VeracodeAPI.exe" # Renomei para remover o # do nome
+Remove-Item "$caminhoDownload" # Remove o arquivo de download
 
 # Utilizacao das ferrementas:
 VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action UploadAndScan -appname "$NomeApp" -filepath "$caminhoArquivo" -version $numeroVersao # API Wrapper
--- Windows/Terminal.ps1
+++ Windows/Terminal.ps1
@@ -4,24 +4,9 @@ srcclr scan --ws=D77HraA # Executa o scan
 
 
 # Pipeline Scan
-# Download e configuracao
-$pastaferramenta  = "$Env:Programfiles/Veracode/PipelineScan" # Define uma pasta onde vamos colocar a ferramenta
-$urlPipeScan = "https://downloads.veracode.com/securityscan/pipeline-scan-LATEST.zip" # Define a url de download
-Invoke-WebRequest -Uri "$urlDownloadAPI" -OutFile "$env:LOCALAPPDATA/VeracodeAPI.zip" # Faz o download
-Expand-Archive -Path "$env:userprofile/Download/pipescan.zip" -DestinationPath "$pastaferramenta" # Descompacta o ZIP para uma pasta
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$pastaferramenta") # Adiciona o caminho no Path do sistema
-# Utilizacao
 java -jar "pipeline-scan.jar" -vid $veracodeID -vkey $veracodeAPIkey -f $caminhoarquivo 
 
 
 # API Wrapper
-# Download e configuracao
-$pastaferramenta  = "$Env:Programfiles/Veracode/API/.NET" # Define uma pasta onde vamos colocar a ferramenta
-$urlDownloadAPI = "https://tools.veracode.com/integrations/API-Wrappers/C%23/bin/VeracodeC%23API.zip" # Define a url de download
-Invoke-WebRequest -Uri "$urlDownloadAPI" -OutFile "$env:LOCALAPPDATA/VeracodeAPI.zip" # Faz o download
-Expand-Archive -Path "$env:LOCALAPPDATA/VeracodeAPI.zip" -DestinationPath "$pastaferramenta" # Descompacta o ZIP para uma pasta
-Rename-Item -Path "$pastaferramenta/VeracodeC#API.exe" -NewName "$pastaferramenta/VeracodeAPI.exe" # Renomei para remover o # do nome
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$pastaferramenta") # Adiciona o caminho no Path do sistema
-# Utilizacao
 $numeroVersao = Get-Date -Format hhmmssddMMyy # Cria um hash com base no dia e hora para numero de versão
 VeracodeAPI.exe -vid $veracodeID -vkey $veracodeAPIkey -action UploadAndScan -appname "$NomeApp" -filepath "$caminhoArquivo" -version $numeroVersao
