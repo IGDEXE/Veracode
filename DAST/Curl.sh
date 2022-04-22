98250fa8a718872c2a60c7cc45355b7246093a8d
Ivo Dias <igd753@gmail.com>
2022-04-22 16:59:16 -0300

    DAST Test

---
+++ DAST/Curl.sh
@@ -0,0 +1,28 @@
+# Configuracoes de uso
+VERACODE_ID=""
+VERACODE_KEY=""
+
+https://api.veracode.com/was/configservice/v1/
+
+# Funcao para autenticar
+aut_Veracode () {
+    # Entrada de dados para a criacao do HMAC Header
+    URLPATH=$1
+    METHOD=$2
+
+    # Faz a criacao
+    NONCE="$(cat /dev/random | xxd -p | head -c 32)"
+    TS="$(($(date +%s%N)/1000))"
+    encryptedNonce=$(echo "$NONCE" | xxd -r -p | openssl dgst -sha256 -mac HMAC -macopt hexkey:$VERACODE_KEY | cut -d ' ' -f 2)
+    encryptedTimestamp=$(echo -n "$TS" | openssl dgst -sha256 -mac HMAC -macopt hexkey:$encryptedNonce | cut -d ' ' -f 2)
+    signingKey=$(echo -n "vcode_request_version_1" | openssl dgst -sha256 -mac HMAC -macopt hexkey:$encryptedTimestamp | cut -d ' ' -f 2)
+    DATA="id=$VERACODE_ID&host=api.veracode.com&url=$URLPATH&method=$METHOD"
+    signature=$(echo -n "$DATA" | openssl dgst -sha256 -mac HMAC -macopt hexkey:$signingKey | cut -d ' ' -f 2)
+    VERACODE_AUTH_HEADER="VERACODE-HMAC-SHA-256 id=$VERACODE_ID,ts=$TS,nonce=$NONCE,sig=$signature"
+}
+
+# Inicia o DAST
+URLPATH=/was/configservice/v1/analyses
+METHOD=POST
+aut_Veracode $URLPATH $METHOD
+curl -s -X $METHOD -H "Authorization: $VERACODE_AUTH_HEADER" "https://api.veracode.com$URLPATH" "Content-Type: application/json" -d "config.json"
